c633e9c65f64e49890b90efce828d31a5bd5a695
AH-dark <ahdark@outlook.com>
2022-08-09 21:36:56 +0800

    Feat: HTTP over TLS support

--- main.go
+++ main.go
@@ -3,22 +3,32 @@ package main
 import (
 	"flag"
 	"fmt"
-
 	"github.com/Ungine-Tech/redirector/bootstrap"
 	"github.com/Ungine-Tech/redirector/router"
 	"github.com/gin-gonic/gin"
 )
 
 var (
-	target string
-	port   string
-	dev    bool
+	target      string
+	port        string
+	dev         bool
+	enableHttps bool
+	httpsPort   string
+	pemFile     string
+	keyFile     string
 )
 
 func init() {
-	flag.StringVar(&target, "t", "http://localhost", "target url")
-	flag.StringVar(&port, "p", ":80", "port")
 	flag.BoolVar(&dev, "D", false, "dev mode")
+	flag.StringVar(&target, "t", "http://localhost", "target url")
+
+	flag.StringVar(&port, "p", ":80", "port")
+
+	flag.BoolVar(&enableHttps, "s", false, "enable https")
+	flag.StringVar(&httpsPort, "sp", ":443", "https port")
+	flag.StringVar(&pemFile, "pem", "", "pem file")
+	flag.StringVar(&keyFile, "key", "", "key file")
+
 	flag.Parse()
 
 	if !dev {
@@ -31,7 +41,23 @@ func init() {
 func main() {
 	r := router.InitRouter()
 
-	fmt.Printf("Server is running on port %s", port)
+	go func(port string) {
+		if enableHttps {
+			if pemFile == "" || keyFile == "" {
+				panic("pem file or key file is empty")
+				return
+			}
+
+			fmt.Printf("TLS Server is running on port %s\n", port)
+			err := r.RunTLS(port, pemFile, keyFile)
+			if err != nil {
+				fmt.Print("error: ", err)
+				return
+			}
+		}
+	}(httpsPort)
+
+	fmt.Printf("Server is running on port %s\n", port)
 	err := r.Run(port)
 	if err != nil {
 		fmt.Print("error: ", err)
